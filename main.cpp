b0777db3fba1f4e5964b02676dd57166c7f42e87
xTheCoderx <justaguyoney@gmail.com>
2021-10-15 16:01:38 +1300

    In the function 'ReverseString()', String 'str' will be converted to a vector of chars.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
+
+void ReverseString(const std::string& str) {
+    std::vector<char> vector;
+
+    for (char character : str) {
+        vector.push_back(character);
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
