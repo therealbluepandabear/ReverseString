034041a835ffbc506a3f537d9051b94f41be5f6f
xTheCoderx <justaguyoney@gmail.com>
2021-10-15 16:08:01 +1300

    ReverseString() can now reverse any string. It will return a string which you can read using std::cout.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <vector>
-
-void ReverseString(const std::string& str) {
+ std::string ReverseString(const std::string& str) {
     std::vector<char> vector;
 
     for (char character : str) {
         vector.push_back(character);
     }
+
+    char reversed[vector.size()];
+
+    int index = (int)vector.size() - 1;
+
+    for (char i : vector) {
+        reversed[index] = i;
+        index--;
+    }
+
+    return reversed;
 }
 
 int main() {
+    std::cout << ReverseString("Hello");
     return 0;
 }
